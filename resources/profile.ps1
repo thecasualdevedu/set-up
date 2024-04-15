da79c4f648b26b7251466bf57bf4b3e25ec7368d
Jack <thecasualdev@proton.me>
2024-04-15 13:51:25 +1000

    updated the ps1 file

--- resources/profile.ps1
+++ resources/profile.ps1
@@ -1,3 +1,5 @@
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/emodipt-extend.omp.json" | Invoke-Expression
 $Host.UI.RawUI.WindowTitle = 'Hello World!'
-cls
+Clear-Host
+
+# main theme : emodipt-extend
